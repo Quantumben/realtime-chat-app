a96ebf0cb598e26bbc345a2aecb17451929f36af
QuantumBen <omosabenedict@gmail.com>
2023-05-30 08:44:21 +0100

    sendMessage method updated completely

--- resources/js/components/ChatComponent.vue
+++ resources/js/components/ChatComponent.vue
@@ -1,28 +1,41 @@
 <template>
     <div class="chat-container">
-      <div class="messages">
-        <div v-for="message in messages" :key="message.id" class="message">
-          <strong>{{ message.user }}</strong>: {{ message.content }}
+        <div class="messages">
+            <div v-for="message in messages" :key="message.id" class="message">
+                <strong>{{ message.user }}</strong
+                >: {{ message.content }}
+            </div>
         </div>
-      </div>
-      <div class="input-area">
-        <input v-model="newMessage" @keyup.enter="sendMessage" type="text" placeholder="Type your message here...">
-      </div>
+        <div class="input-area">
+            <input
+                v-model="newMessage"
+                @keyup.enter="sendMessage"
+                type="text"
+                placeholder="Type your message here..."
+            />
+        </div>
     </div>
-  </template>
+</template>
 
-  <script>
-  export default {
+<script>
+export default {
     data() {
-      return {
-        messages: [],
-        newMessage: '',
-      };
+        return {
+            messages: [],
+            newMessage: "",
+        };
     },
     methods: {
-      sendMessage() {
-        // Logic to send a new message will go here
-      },
+        async sendMessage() {
+            if (this.newMessage.trim() === "") return;
+
+            const response = await axios.post("/send-message", {
+                content: this.newMessage,
+            });
+            this.messages.push(response.data);
+            this.newMessage = "";
+            this.socket.emit("send-message", response.data);
+        },
     },
-  };
-  </script>
+};
+</script>
